509133ebf289b3a3a3b5de1c3e526ba2f0de38b2
kazoo910 <yoneyama.kazoo@gmail.com>
2025-08-04 07:08:10 +0000

    「語尾ににゃ～を追加」

--- src/View/Posts/index.php
+++ src/View/Posts/index.php
@@ -53,7 +53,7 @@
                             </div>
                             <div class="post-info" data-id="<?=$post['id']?>">
                                 <input type="text" class="post-name post-not-edit-input" value=<?=$post['name']?> readonly><br>
-                                <textarea class="post-text post-not-edit-textarea" readonly> <?=$post['message']?> </textarea>
+                                <textarea class="post-text post-not-edit-textarea" readonly> <?=$post['message']?> にゃ～ん </textarea>
                                 <div class="post-action">
                                     <button type="button" class="post-action-btn edit-btn" onclick="editPost(this)">✒️編集</button>
                                     <button type="button" class="post-action-btn delete-btn" onclick="deletePost(this)">🗑削除</button>
